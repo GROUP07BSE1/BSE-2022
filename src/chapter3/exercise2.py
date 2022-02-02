5c38821bd31cb4255c199e82d60b5b4543a5a674
BSE GROUP 20 <tukasiimablessing43@gmail.com>
2022-02-02 18:36:54 +0300

    exercise2

---
+++ src/chapter3/exercise2.py
@@ -0,0 +1,16 @@
+try:
+    working_hours = float(input("enter hours: "))
+    rate = float(input("enter rate: " ))
+    pay = working_hours * rate
+    if working_hours < 40:
+        print(pay)
+    else:
+        basic_pay = 400
+        excess_rate = working_hours - 40
+        bonus = excess_rate * rate * 1.5
+        pay2 = basic_pay + bonus
+        print(pay2)
+
+except ValueError:
+    print("invalid input")
+
---
+++ src/chapter3/exercise3.py
@@ -0,0 +1,49 @@
+try:
+    score = float(input("score"))
+    if score > 1 or score < 0:
+        raise ValueError
+    if score >= 0.9:
+        Grade = "A"
+        print(Grade)
+    elif score >= 0.8:
+        Grade = "B"
+        print(Grade)
+    elif score >= 0.7:
+        Grade = "C"
+        print(Grade)
+    elif score >= 0.6:
+        Grade = "D"
+        print(Grade)
+    else:
+        print("F")
+except ValueError:
+    print("bad score")
+
+
+
+
+
+
+
+
+
+
+    # if score >= 0.9:
+    #     Grade = "A"
+    #     print(Grade)
+    # elif score >= 0.8:
+    #     Grade = "B"
+    #     print(Grade)
+    # elif score >= 0.7:
+    #     Grade = "C"
+    #     print(Grade)
+    # elif score >= 0.6:
+    #     Grade = "D"
+    #     print(Grade)
+    # else:
+    #     Grade = "F"
+    #     print("Grade")
+    # print("bad score")
+
+
+
